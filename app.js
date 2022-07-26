a494186ddedf81ec1ddeb13991352d7faecd64cb
wicfasho <adubiaranwesley@gmail.com>
2022-07-26 14:32:26 +0100

    Init Windows Service Auto Restart

---
+++ app.js
@@ -0,0 +1,6 @@
+// @Author Adu Wesley Young
+// @LinkedIn https://linkedin.com/wicfasho
+// @Github https://github.com/wicfasho //99.99% of my projects are Private (LOL)
+// @Licence NONE
+// @Problem Service Downtime was somthing we contantly battled at work due to some processes not being scaled. The solution was to restart the window service where the application runs. I am too Lazy to always have to RDP to different servers to restart their services.
+// @Solution #Description As a lover of automations, i decided to automate this process by pinging the website in intervals and restart when not reachable.
---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sites: [
+    {
+        "name": "Apache Tomcat Lucee",
+        "uri": "http://localhost/ysr-iis/dashboard",
+        "window_service_name": "Lucee",
+        "check_interval": 1000 * 60 //1 minutes
+    }
+  ]
+};

4d99bfc3c7e9e5ff06bbe5c74a36f822cc2edc0d
wicfasho <adubiaranwesley@gmail.com>
2022-10-29 05:24:37 +0100

    Restructed how multiple sites are being monitored and the general logic.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// @Author Adu Wesley Young
+// @Author Wicfasho
 // @LinkedIn https://linkedin.com/wicfasho
 // @Github https://github.com/wicfasho //99.99% of my projects are Private (LOL)
 // @Licence NONE
@@ -61,14 +61,14 @@ async function runCMD(command){
             if (error !== null) {
                 result = {
                     "status": "error",
-                    "description": String(error), //.replaceAll('\r','').replaceAll('\n','')
+                    "description": String(error).replaceAll('\r','').replaceAll('\n',''),
                     "stderr": String(stderr)
                 }
             }
             else{
                 result = {
                     "status": "success",
-                    "description": String(stdout)
+                    "description": String(stdout).replaceAll('\r','').replaceAll('\n',''),
                 }
             }
             resolve(result)
@@ -78,35 +78,34 @@ async function runCMD(command){
 
 function P_RESTART(site,service_name){
     let service_type = (service_name == "w3svc") ? "iis" : "app_service";
-    let restart_wait_time = (service_type == "iis") ? 6 : 15; //seconds
+    let restart_wait_time = (service_type == "iis") ? 6 : 60 * 4; //seconds
     let restart_count = site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].restart_count[service_type];
     let machine_name = String.raw`\\${site.hostname}`;
     return new Promise( async (resolve,reject) => {
         var check_service = await runCMD(`SC \\\\${site.hostname} QUERYEX ${service_name} | FIND "STATE" | FIND /v "RUNNING" > NUL && (echo 0) || (echo 1)`)
         if(check_service.status == "success" && Number(check_service.description) == 1){ //RUNNING
-            // @notice Max restart for IIS is 2 times
+            // @notice Max restart for IIS is 2 times (from when service restarts)
             if(restart_count <= 2 || service_type == "app_service"){
                 let stop_service = await runCMD(`SC ${machine_name} STOP ${service_name}`)
                 if(stop_service.status == "success"){
                     log(`Service for [${site.hostname}] was stopped`, stop_service.description)
-                    var done = (function wait () { if (!done) setTimeout(wait, 1000) })();
-
-                    // @notice wait some seconds before attempting to start 
+                    
+                    // @notice wait some seconds before attempting to start
+                    log(`Waiting for ${restart_wait_time} seconds before restarting...`.yellow)
                     setTimeout( async () => {
                         let start_service = await runCMD(`SC ${machine_name} START ${service_name}`)
                         if(start_service.status == "success"){
                             log(`Service for [${site.hostname}] has started`, start_service.description)
                             restart_count = ++site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].restart_count[service_type]
-                            done = true
+
                             resolve("success")
                         }else{
-                            done = true;
-                            resolve("failure")
+                            resolve("failure.couldnotrestart.successfullystopped")
                         }
                     }, 1000 * restart_wait_time)
 
                 }else{
-                    resolve("failure")
+                    resolve("failure.couldnotstop.alreadystarted")
                 }
             }else{
                 resolve("success")
@@ -118,116 +117,163 @@ function P_RESTART(site,service_name){
                 restart_count = ++site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].restart_count[service_type]
                 log(`Service for [${site.hostname}] has started`, start_service.description)
                 resolve("success")
+            }else{
+                resolve("failure.couldnotstart.alreadystopped")
             }
         }else{
-            resolve("failure")
+            resolve("failure.unknownreason")
         }
     });
 }
 
 function restartService(site){
-    let uri = site.uri;
-    let service = site.window_service_name;
-    let failure_count_before_restart = site.failure_count_before_restart;
+    return new Promise ( async (resolve, reject) => {
+        let uri = site.uri;
+        let service = site.window_service_name;
+        let failure_count_before_restart = site.failure_count_before_restart;
 
-    let restart_in_progress = site_state_variables.filter(element => element.uri == uri)[0].restart_in_progress;
-    let failure_count = site_state_variables.filter(element => element.uri == uri)[0].failure_count;
+        let restart_in_progress = site_state_variables.filter(element => element.uri == uri)[0].restart_in_progress;
+        let failure_count = site_state_variables.filter(element => element.uri == uri)[0].failure_count;
 
-    if(Number(failure_count) >= Number(failure_count_before_restart)){
-        console.log(restart_in_progress) //
-        if(!restart_in_progress){
-            restart_in_progress = site_state_variables[site_state_variables.findIndex(element => element.uri == uri)].restart_in_progress = true;
-            failure_count = site_state_variables[site_state_variables.findIndex(element => element.uri == uri)].failure_count = 0; //reset failure count
+        if(Number(failure_count) >= Number(failure_count_before_restart)){
+            if(!restart_in_progress){
+                restart_in_progress = site_state_variables[site_state_variables.findIndex(element => element.uri == uri)].restart_in_progress = true;
+                failure_count = site_state_variables[site_state_variables.findIndex(element => element.uri == uri)].failure_count = 0; //reset failure count
 
-            let P_IIS = P_RESTART(site,'w3svc')
-            let P_APP = P_RESTART(site,service)
-            var done = (function wait () { if (!done) setTimeout(wait, 1000) })();
-            console.log("i got here") //code stopping here after restarting on line 214
-            Promise.all([P_IIS,P_APP]).then((returns) => {
-                done = true;
-                restart_in_progress = site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].restart_in_progress = false;
-                console.log(returns)
-                let all_successful = true;
-                for(let i = 0; i<returns.length; i++){
-                    if( returns[i].trim().toLowerCase() == "failure" ) {
-                        all_successful = false;
-                        restartService(site);
+                const allRestart = await Promise.all([
+                    P_RESTART(site,'w3svc'),
+                    P_RESTART(site,service)
+                ])
+                
+                let all_restart_successful = true;
+                for(let i = 0; i<allRestart.length; i++){
+                    if( allRestart[i].trim().toLowerCase() != "success" ) {
+                        all_restart_successful = false;
                         break;
                     }
                 }
 
-                if(all_successful){
+                restart_in_progress = site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].restart_in_progress = false;
+                if(all_restart_successful){
                     log(site.hostname + " RESTARTED!!! I DIT IT! \n\n".bright.green);
-                    setTimeout( () => { start(); }, 2000)
+
+                    const wait_seconds = 60;
+                    log(`Waiting ${wait_seconds} seconds before checking again...`.yellow)
+                    setTimeout( function(){
+                        resolve({
+                            status: 'success',
+                            response: 'Service Restarted',
+                            data: allRestart
+                        })
+                    }, wait_seconds * 1000)
+                }else{
+                    resolve({
+                        status: 'failure',
+                        response: 'Could not restart the services',
+                        data: allRestart
+                    })
                 }
-            });
+            }
+            else{
+                resolve({
+                    status: 'failure.restartalreadyinprogress',
+                    response: 'Restart already in progress'
+                })
+            }
+        }else{
+            resolve({
+                status: 'failure.lesscount',
+                response: 'Failure Count less than required failures that can occur before restart'
+            })
         }
-        else{
-            //@notice There is pending restart. @comment Thanks for trying. You were not as fast.
-        }
-    }else{
-        start();
-    }
+    })
 }
 
-function start(){
-    hosts.forEach( (host) => {
-        host.trim();
-
-        var site = config.sites.filter(element => element.uri.trim() == host)[0];
-
-        const request = http.get(host, optionsGET, (res) => {
-            if( res.statusCode == 200 ){
-                site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].failure_count = 0;
-                log(`[Web] ${host} is alive`.green);
-                
-                request.shouldKeepAlive = false;
-                console.log("Heartbeat...\n");
-                setTimeout( () => {
-                    start();
-                }, site.check_interval)
+function httpGetRequest(host, options, site) {
+    return new Promise(function(resolve, reject) {
+        var request = http.get(host, options, function(response) {
+            if( response.statusCode == 200 ){
+                setStatus = 'success'
             }else{
-                log(`[Web] ${host} is dead`.red);
-                let failure_count = ++site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].failure_count; // @update state variable
-
-                var log_msg = `Restarting the service [${site.window_service_name}]...${failure_count}`;
-                console.log(log_msg);
                 saveLog({
                     "date": Date(),
-                    "msg": `Host is not reachable. Status Code is ${res.statusCode}`
+                    "msg": `Host is not reachable. Status Code is ${response.statusCode}`
                 })
-                setTimeout( () => { restartService(site); }, 1000)
+                setStatus = 'failure'
             }
-        })
 
+            resolve({
+                status: setStatus,
+                response,
+            })
+        });
+        
         request.on('timeout', () => {
-            let failure_count = ++site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].failure_count;
-
             var log_msg = `TIMEOUT: Can not reach host ${host}`;
             log(log_msg.red)
-            saveLog({
+
+            let response = {
                 "date": Date(),
                 "msg": log_msg
+            }
+
+            saveLog(response)
+            resolve({
+                status: 'error',
+                response,
             })
-            request.shouldKeepAlive = false;
-            console.log(`Restarting the service [${site.window_service_name}]...${failure_count}\n`);
-            setTimeout( () => { restartService(site); }, 5000)
         });
 
         request.on('error', function(e){
-            let failure_count = ++site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].failure_count;
-
             var log_msg = `ERROR: Can not reach host ${host}`;
             log(log_msg.red)
-            saveLog({
+
+            let response = {
                 "date": Date(),
                 "msg": log_msg
+            }
+
+            saveLog(response)
+            resolve({
+                status: 'error',
+                response,
             })
-            request.shouldKeepAlive = false;
-            console.log(`Restarting the service [${site.window_service_name}]...${failure_count}\n`);
-            setTimeout( () => { restartService(site); }, 5000)
         });
+        
+        request.end()
     });
 }
 
-start();
+(async function start(){
+    var server_check_count = 0;
+
+    for(let i=0; i<hosts.length; i++){
+        server_check_count++;
+        var host = hosts[i].trim();
+        var site = config.sites.filter(element => element.uri.trim() == host)[0];
+
+        const request = await httpGetRequest(host, optionsGET, site)
+        if(request.status == 'success'){
+            site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].failure_count = 0;
+            log(`[${site.hostname}] ${host} is alive`.green);
+
+            if(server_check_count >= hosts.length){
+                setTimeout( async () => {
+                    start()
+                }, site.check_interval)
+            }
+        }else{
+            ++site_state_variables[site_state_variables.findIndex(element => element.uri == site.uri)].failure_count;
+            log(`[${site.hostname}] ${host} is dead`.red);
+
+            const restart_service = await restartService(site);
+            log(restart_service)
+            if(server_check_count >= hosts.length){
+                setTimeout( async () => {
+                    start()
+                }, site.check_interval)
+            }
+            console.log('\r\n')
+        }        
+    }
+})()
--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   sites: [
     {
-        "name": "Apache Tomcat Lucee",
-        "uri": "http://localhost/ysr-iis/dashboard",
-        "hostname": "WESLEY-PC",  //run "hostname" via cmd/powershell to get
-        "window_service_name": "Lucee",
-        "check_interval": 1000 * 7, //7 seconds
-        "failure_count_before_restart": 3
+      "name": "",
+      "uri": "",
+      "hostname": "",  //run "hostname" via cmd/powershell to get
+      "window_service_name": "",
+      "check_interval": 1000 * 7, //7 seconds
+      "failure_count_before_restart": 3
     }
   ]
 };
